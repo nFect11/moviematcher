29f71e6d357e5ea20454beeaaecf6c191ad4a3f3
ferdibaum <ferdi99@live.de>
2021-10-21 19:55:45 +0200

    fixed merge

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,34 +7,6 @@ import MovieList from "./movies/MovieList";
 import { RoomDisplay } from "./RoomDisplay";
 
 export default function Menu() {
-<<<<<<< Updated upstream
-    const [step, changeStep] = useState(0)
-    const [config, changeConfig] = useState(true)
-    
-    const handlePrevious = () => {
-        if(step>0){
-        changeStep(step - 1)}
-    }
-    const handleNext = () => {
-        changeStep(step + 1)
-        if(step===2){
-            changeConfig(false)
-        }
-    }
-    return <div className="h-1/2"><div>
-        {(() => {switch(step) {
-        case 0: return <Landing />;
-        case 1: return <GenreSelector />;
-        case 2: return <GenreSelectorHate />;
-        case 3: return <MovieList />;
-        default: <div></div>}})()}
-        </div>
-        {config && <button className={`absolute top-10 bg-gray-400 w-28 cursor-pointer ${step===0 && 'hidden'}`} onClick={handlePrevious}>Previous</button>}
-        {config && <button className={`absolute top-10 ml-48 bg-gray-400 w-28 `} onClick={handleNext}>{step === 2 ? "Finish" : "Next"}</button>}
-        
-    </div>
-}
-=======
   const [step, changeStep] = useState(0);
 
   const handlePrevious = () => {
@@ -84,4 +56,3 @@ export default function Menu() {
     </RoomProvider>
   );
 }
->>>>>>> Stashed changes
