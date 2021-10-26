07fffbc1e33ff243b1bd3cc7d03ab61e31208833
Arda <ardasayin03@gmail.com>
2021-10-27 00:21:51 +0200

    Tooltip added

--- src/components/RoomDisplay.js
+++ src/components/RoomDisplay.js
@@ -1,24 +1,71 @@
-import { useContext } from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useContext, useEffect, useState } from "react";
 import { RoomContext } from "../contexts/roomContext";
+import GenreContext from "./store/genre-context";
+import ReactTooltip from "react-tooltip";
 
 export function RoomDisplay() {
   const { room } = useContext(RoomContext);
+  const genreCtx = useContext(GenreContext);
+
+  function voterList(votedMovie) {
+    let nameList = "";
+    votedMovie.voter.forEach((voterName) => {
+      for (let i = 0; i < room?.users.length; i++) {
+        if (voterName === room?.users[i].id) {
+          nameList += `${room?.users[i].name}<br />`;
+        }
+      }
+    });
+    return nameList;
+  }
+  useEffect(() => {
+    ReactTooltip.rebuild();
+  });
 
   function getListWithVoteCounts(movies) {
-    if (!movies) return {};
-    let movieswithVoteCounts = {};
+    if (!movies) return [];
+    let existsFlag = false;
+    let movieswithVoteCounts = [];
+
+    //for (let i = 0; i < movies.length; i++) {
+    //  if (movieswithVoteCounts.hasOwnProperty(movies[i].id)) {
+    //    movieswithVoteCounts[movies[i].id]["votes"] =
+    //      movieswithVoteCounts[movies[i].id]["votes"] + 1;
+    //  } else {
+    //    movieswithVoteCounts[movies[i].id] = {
+    //      votes: 1,
+    //      img: movies[i].poster_path,
+    //    };
+    //  }
+    //}
 
     for (let i = 0; i < movies.length; i++) {
-      if (movieswithVoteCounts.hasOwnProperty(movies[i].id)) {
-        movieswithVoteCounts[movies[i].id]["votes"] =
-          movieswithVoteCounts[movies[i].id]["votes"] + 1;
-      } else {
-        movieswithVoteCounts[movies[i].id] = {
-          votes: 1,
-          img: movies[i].poster_path,
-        };
+      existsFlag = false;
+      for (let j = 0; j < movieswithVoteCounts.length; j++) {
+        if (movieswithVoteCounts[j].id === movies[i].id) {
+          if (!movieswithVoteCounts[j].voter.includes(movies[i].voter)) {
+            movieswithVoteCounts[j].voter.push(movies[i].voter);
+          }
+          existsFlag = true;
+          break;
+        }
       }
+      !existsFlag &&
+        movieswithVoteCounts.push({
+          id: movies[i].id,
+          img: movies[i].poster_path,
+          voter: [genreCtx.userId],
+        });
     }
+    movieswithVoteCounts.sort((a, b) =>
+      a.voter.length > b.voter.length
+        ? 1
+        : b.voter.length > a.voter.length
+        ? -1
+        : 0
+    );
+    movieswithVoteCounts.reverse();
     return movieswithVoteCounts;
   }
   const imgPath = "https://image.tmdb.org/t/p/original";
@@ -26,30 +73,39 @@ export function RoomDisplay() {
     return <div></div>;
   } else {
     return (
-      <div className="absolute top-0 right-0 font-bold text-white pt-36 pr-36">
+      <div className="absolute top-0 right-0 font-bold text-white mt-36 mr-36 w-1/5">
         <div>Room: {room?.id}</div>
         <div>Users: </div>
+
         {room?.users.map((user, index) => (
           <div key={index}>{user.name}</div>
         ))}
-
-        <div>Movies: </div>
-        {Object.keys(getListWithVoteCounts(room?.movieScoreList)).map(
-          (movie, index) => (
+        <section>
+          <div>Movies: </div>
+          {getListWithVoteCounts(room?.movieScoreList).map((movie, index) => (
             <div key={index}>
-              <img
-                className="w-24"
-                src={`${imgPath}${
-                  getListWithVoteCounts(room?.movieScoreList)[movie]["img"]
-                }`}
-                alt={"alt"}
-              />
-              <div>
-                {getListWithVoteCounts(room?.movieScoreList)[movie]["votes"]}
-              </div>
+              <button
+                data-for="main"
+                data-tip={voterList(movie)}
+                data-iscapture="true"
+              >
+                <img
+                  className="w-1/5"
+                  src={`${imgPath}${movie.img}`}
+                  alt={"alt"}
+                />
+              </button>
+              <div>{movie.voter.length}</div>
             </div>
-          )
-        )}
+          ))}
+          <ReactTooltip
+            id="main"
+            place="right"
+            type="dark"
+            effect="float"
+            multiline={true}
+          />
+        </section>
       </div>
     );
   }
